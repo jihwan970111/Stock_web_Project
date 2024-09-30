2361f758377b5786b780f35a37baed2a74213414
jihwan970111 <jihwan970111@gmail.com>
2024-09-30 09:44:35 +0900

    Initial commit

---
+++ Stock_Project/src/main/java/kr/ac/kopo/ctc/stock/StockApplication.java
@@ -0,0 +1,12 @@
+package kr.ac.kopo.ctc.stock;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class StockApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(StockApplication.class, args);
+	}
+
+}
